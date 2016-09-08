5a9876c2975ee04ede5028bae498b7d0823e3053
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-09-08 19:47:31 +0200

    Translate app name

--- froide_campaign/apps.py
+++ froide_campaign/apps.py
@@ -1,10 +1,11 @@
 # -*- encoding: utf-8 -*-
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class FroideCampaignConfig(AppConfig):
     name = 'froide_campaign'
-    verbose_name = "Froide Campaign App"
+    verbose_name = _("Froide Campaign App")
 
     def ready(self):
         from .listeners import connect_info_object
