2dbe8587ac49566a5bbcfc8d3d2d37586163b305
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-02-01 19:43:54 +0100

    Register menu item

--- froide_campaign/apps.py
+++ froide_campaign/apps.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from django.apps import AppConfig
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -12,3 +13,15 @@ class FroideCampaignConfig(AppConfig):
 
         from froide.foirequest.models import FoiRequest
         FoiRequest.request_created.connect(connect_info_object)
+
+        from froide.account.menu import menu_registry, MenuItem
+
+        @menu_registry.register
+        def get_campaign_menu_item(request):
+            if not request.user.has_perm('froide_campaign.can_use_campaigns'):
+                return None
+            return MenuItem(
+                section='before_settings', order=10,
+                url=reverse('campaign-list'),
+                label=_('Your campaigns')
+            )
