ac88a1d2b5c31e85bdd6789bc9d899129f83e53f
Magdalena Noffke <magdalena.noffke@googlemail.com>
2020-10-06 19:38:42 +0200

    Fix template tags

--- froide_campaign/templatetags/campaign_tags.py
+++ froide_campaign/templatetags/campaign_tags.py
@@ -45,3 +45,20 @@ def script_tag(path):
         '<script src="{url}" integrity="{sri}" crossorigin="anonymous" '
         'async></script>'.format(url=url, sri=sri)
     )
+
+
+@register.filter
+def request_population_ratio(value):
+    if isinstance(value, dict):
+        req = value['request_count']
+        pop = value['population']
+    else:
+        req = value.request_count
+        pop = value.population
+    return round(req / pop * 100_000, 1)
+
+
+@register.filter
+def in_mio(value):
+    if value:
+        return round(value / 1_000_000, 2)
