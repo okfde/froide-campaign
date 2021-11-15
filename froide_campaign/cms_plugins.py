d6ecc7a62e7df19540ef3518d2077b8b0a27bfae
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-11-15 10:51:09 +0100

    Refactor campaign-list cms plugin render

--- froide_campaign/cms_plugins.py
+++ froide_campaign/cms_plugins.py
@@ -2,7 +2,6 @@ import json
 import logging
 
 from django.core.serializers.json import DjangoJSONEncoder
-from django.templatetags.static import static
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import get_language
 
@@ -252,33 +251,30 @@ class CampaignListPlugin(CMSPluginBase):
 
         lang = get_language()
 
-        campaign = instance.campaign.id
+        campaign = instance.campaign
         plugin_settings = instance.settings
-        law_type = None
-        try:
-            law_type = instance.campaign.provider_kwargs.get('law_type')
-        except AttributeError:
-            pass
 
-        categories = instance.campaign.categories.language(lang)
+        categories = campaign.categories.language(lang)
         categories_dict = [{'id': cat.id, 'title': cat.title}
                            for cat in categories]
 
         config = {
-            'campaignId': campaign,
-            'lawType': law_type,
+            'campaignId': campaign.id,
             'categories': categories_dict,
             'requestExtraText': instance.request_extra_text,
         }
-        fake_make_request_view = MakeRequestView(request=request)
+        # Add provider kwargs to config
+        config.update(instance.campaign.provider_kwargs)
+
+        make_request_view = MakeRequestView(request=request)
 
         context.update({
             'config': json.dumps(config),
             'settings': json.dumps(plugin_settings),
             'request_config': json.dumps(
-                fake_make_request_view.get_js_context()),
-            'request_form': fake_make_request_view.get_form(),
-            'user_form': fake_make_request_view.get_user_form(),
+                make_request_view.get_js_context()),
+            'request_form': make_request_view.get_form(),
+            'user_form': make_request_view.get_user_form(),
             'language': lang,
         })
         return context
